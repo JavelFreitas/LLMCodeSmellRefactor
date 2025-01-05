91b7bac0152320294f97f72cdca4b56509daad7d
Lucas Tito <lucassga500@gmail.com>
2025-01-04 22:31:28 -0300

    fix: refactor code smell feauture envy using move method refactoring technique

--- src/main/java/org/example/studysearch/SearchLog.java
+++ src/main/java/org/example/studysearch/SearchLog.java
@@ -1,5 +1,10 @@
 package org.example.studysearch;
 
+import org.example.studycards.CardManager;
+import org.example.studyplanner.HabitTracker;
+import org.example.studyplanner.TodoTracker;
+import org.example.studyregistry.StudyMaterial;
+import org.example.studyregistry.StudyTaskManager;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -57,5 +62,22 @@ public class SearchLog {
 
     public void setLogName(String logName) {
         this.logName = logName;
+    }
+
+    public void logSearch(String searchText) {
+        this.addSearchHistory(searchText);
+        this.numUsages++;
+    }
+
+    public List<String> handleSearch(String text) {
+        List<String> results = new ArrayList<>();
+        results.addAll(CardManager.getCardManager().searchInCards(text));
+        results.addAll(HabitTracker.getHabitTracker().searchInHabits(text));
+        results.addAll(TodoTracker.getInstance().searchInTodos(text));
+        results.addAll(StudyMaterial.getStudyMaterial().searchInMaterials(text));
+        results.addAll(StudyTaskManager.getStudyTaskManager().searchInRegistries(text));
+        this.logSearch(text);
+        results.add("\nLogged in: " + this.getLogName());
+        return results;
     }
 }
