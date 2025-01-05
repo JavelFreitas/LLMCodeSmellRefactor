9f799069747e20b8c56535c3c3c7aa1764dc3b62
Elenildo Filho <elenildovas123@gmail.com>
2025-01-04 23:32:05 -0300

    fix: refactor code smell Data Class 3 using Encapsulation method refactoring technique

--- src/main/java/org/example/studyregistry/Task.java
+++ src/main/java/org/example/studyregistry/Task.java
@@ -1,12 +1,18 @@
 package org.example.studyregistry;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Task extends Registry{
     private String title;
     private String description;
     private String author;
     private LocalDateTime date;
+    private boolean completed;
+    private List<LocalDateTime> reminders = new ArrayList<>();
+    private List<Task> subtasks = new ArrayList<>();
+
 
     public Task(String title, String description, String author, LocalDateTime date) {
         this.title = title;
@@ -38,5 +44,21 @@ public class Task extends Registry{
     }
     public void setDate(LocalDateTime date) {
         this.date = date;
+    }
+
+    public void markAsCompleted() {
+        this.completed = true;
+    }
+
+    public void addReminder(LocalDateTime reminderDate) {
+        reminders.add(reminderDate);
+    }
+
+    public void addSubtask(Task subtask) {
+        subtasks.add(subtask);
+    }
+
+    public void removeSubtask(Task subtask) {
+        subtasks.remove(subtask);
     }
 }
