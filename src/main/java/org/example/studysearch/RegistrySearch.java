5800e415e24b27d62f7493230ecfc1e18d17b487
guilhermepls <guipinheiro17@alu.ufc.br>
2025-01-08 22:17:36 -0300

    fix: refactor code smell Feature Envy using Move Method refactoring technique

--- src/main/java/org/example/studysearch/RegistrySearch.java
+++ src/main/java/org/example/studysearch/RegistrySearch.java
@@ -8,28 +8,23 @@ import org.example.studyregistry.StudyTaskManager;
 import java.util.ArrayList;
 import java.util.List;
 
-public class RegistrySearch implements Search<String>{
-    private SearchLog searchLog = new SearchLog("Registry Search");
-    public RegistrySearch(){}
+public class RegistrySearch implements Search<String> {
+    private final SearchLog searchLog = new SearchLog("Registry Search");
 
     @Override
     public List<String> search(String text) {
-        return handleRegistrySearch(text);
-    }
-
-    public SearchLog getSearchLog() {
-        return searchLog;
-    }
-
-    private List<String> handleRegistrySearch(String text){
         List<String> results = new ArrayList<>();
         results.addAll(CardManager.getCardManager().searchInCards(text));
         results.addAll(HabitTracker.getHabitTracker().searchInHabits(text));
         results.addAll(TodoTracker.getInstance().searchInTodos(text));
         results.addAll(StudyTaskManager.getStudyTaskManager().searchInRegistries(text));
-        this.searchLog.addSearchHistory(text);
-        this.searchLog.setNumUsages(this.searchLog.getNumUsages() + 1);
-        results.add("\nLogged in: " + this.searchLog.getLogName());
+
+        searchLog.logSearchAndIncrement(text);
+        results.add("\n" + searchLog.logSearchAndIncrement(text));
         return results;
+    }
+
+    public SearchLog getSearchLog() {
+        return searchLog;
     }
 }
