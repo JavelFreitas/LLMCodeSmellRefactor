ed22301df324a194a7d08a2347c2db8d3f566d77
Luis Estevam Rosa Chaves <luisestevam33@gmail.com>
2025-01-10 15:55:08 -0300

    fix: refactor code smell Feature Envy using Move Method refactoring technique

--- src/main/java/org/example/studyplanner/HabitTracker.java
+++ src/main/java/org/example/studyplanner/HabitTracker.java
@@ -111,4 +111,18 @@ public class HabitTracker {
         return habits;
     }
 
+    public String generateHabitReport() {
+        StringBuilder response = new StringBuilder();
+        for (Habit habit : this.habits) {
+            response.append("[ Habit: ")
+                    .append(habit.getName())
+                    .append(". Records: ");
+            List<LocalDateTime> records = this.getHabitRecords(habit.getId());
+            for (LocalDateTime record : records) {
+                response.append(this.formatHabitDate(record)).append(", ");
+            }
+            response.append("]");
+        }
+        return response.toString();
+    }
 }
