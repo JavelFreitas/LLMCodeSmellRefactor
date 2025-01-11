b462e9e2fd2574fe50c844741aea7ef1035e85d3
AndersonLima0 <84778277+AndersonLima0@users.noreply.github.com>
2025-01-11 18:09:14 -0300

    fix Long Parament List code smell using the move mehod refatoring technique

--- src/main/java/org/example/controllers/StudyRegistryController.java
+++ src/main/java/org/example/controllers/StudyRegistryController.java
@@ -109,15 +109,36 @@ public class StudyRegistryController {
         studyTaskManager.addRegistry(goal);
     }
 
-    private void editAudio(AudioReference audioReference){
+    private void editAudio(AudioReference audioReference) {
         handleMethodHeader("(Audio Edit)");
-        System.out.println("Type the following info:  AudioReference. AudioQuality audioQuality, boolean isDownloadable, " +
+        System.out.println("Type the following info: AudioReference.AudioQuality audioQuality, boolean isDownloadable, " +
                 "String title, String description, String link, String accessRights, String license, String language, int rating, " +
                 "int viewCount, int shareCount \n");
-        AudioReference.AudioQuality quality =AudioReference.audioQualityAdapter(getInput());
-        audioReference.editAudio(quality, Boolean.parseBoolean(getInput()), getInput(), getInput(), getInput(), getInput(),
-                getInput(), getInput(), Integer.parseInt(getInput()), Integer.parseInt(getInput()), Integer.parseInt(getInput()));
+    
+        AudioReference.AudioQuality quality = AudioReference.audioQualityAdapter(getInput());
+    
+        AudioReference.BasicDetails basicDetails = new AudioReference.BasicDetails(
+                getInput(), // Title
+                getInput(), // Description
+                getInput()  // Link
+        );
+    
+        AudioReference.AudioAttributesConfig config = new AudioReference.AudioAttributesConfig.Builder()
+                .setAudioQuality(quality)
+                .setDownloadable(Boolean.parseBoolean(getInput())) // isDownloadable
+                .setBasicDetails(basicDetails)
+                .setAccessRights(getInput())  // Access Rights
+                .setLicense(getInput())       // License
+                .setLanguage(getInput())      // Language
+                .setRating(Integer.parseInt(getInput()))          // Rating
+                .setViewCount(Integer.parseInt(getInput()))       // View Count
+                .setShareCount(Integer.parseInt(getInput()))      // Share Count
+                .build();
+    
+        AudioReference.AudioAttributes attributes = new AudioReference.AudioAttributes(config);
+        audioReference.editAudio(attributes);
     }
+    
 
     private AudioReference addAudioReference(){
         handleMethodHeader("(Audio Reference Creation)");
